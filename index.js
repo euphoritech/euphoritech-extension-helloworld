ce614a14e3570044d92c317047dfbfa2d25578d0
Lance <whatl3y@gmail.com>
2018-06-06 14:21:02 -0400

    Log params

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
   go(params) {
     // params: the parameters provided by the event firing the extension
-    
+
     const colors = MODULES['colors']
     console.log("Hello World".underline.green)
+    console.log(JSON.stringify(params).blue)
   }
 }
