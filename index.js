4a7259c90b7b0cdedffc94f93b83cf857e3e2e13
Lance <whatl3y@gmail.com>
2018-06-06 14:13:39 -0400

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+{
+  modules: [ 'colors' ],
+
+  go(params) {
+    // params: the parameters provided by the event firing the extension
+    
+    const colors = MODULES['colors']
+    console.log("Hello World".underline.green)
+  }
+}
